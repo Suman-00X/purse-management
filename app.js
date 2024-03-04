be333106dbe5490fe202a9e73dc3f7fea9ea2a63
Suman raj <raj_suman@hotmail.com>
2024-03-04 22:55:52 +0530

    set base url

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
