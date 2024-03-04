e19d202dd1509fce0574fbc05dfd15b91e7cb530
Suman raj <raj_suman@hotmail.com>
2024-03-04 23:00:11 +0530

    set build path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.use('/routes/', diary);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
